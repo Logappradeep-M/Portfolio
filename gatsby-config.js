e8d15f27d663ba7a7cf111054e7e95caa3a3d4c9
Logappradeep <84114284+Logappradeep-M@users.noreply.github.com>
2024-06-16 11:31:10 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Logappradeep',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Logappradeep Pega Senior System Architect',
+    siteUrl: 'https://logappradeep.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@logappradeep',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
