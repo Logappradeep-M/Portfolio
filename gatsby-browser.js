9e23fe1036bde121cc428cc68f5ad528f0f6694f
logappradeep-m <logappradeep@gmail.com>
2024-07-19 16:36:13 +0530

    Files updated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,21 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+// /**
+//  * Implement Gatsby's Browser APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/browser-apis/
+//  */
+const React = require("react");
+
+exports.onClientEntry = () => {
+const script = document.createElement("script");
+script.setAttribute("data-name", "BMC-Widget");
+script.setAttribute("data-cfasync", "false");
+script.setAttribute("src", "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js");
+script.setAttribute("data-id", "logappradep");
+script.setAttribute("data-description", "Support me on Buy me a coffee!");
+script.setAttribute("data-message", "");
+script.setAttribute("data-color", "#40DCA5");
+script.setAttribute("data-position", "Right");
+script.setAttribute("data-x_margin", "18");
+script.setAttribute("data-y_margin", "18");
+document.body.appendChild(script);
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+// /**
+//  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/ssr-apis/
+//  */
 
- // You can delete this file if you're not using it
+//  // You can delete this file if you're not using it
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+setHeadComponents([
+<script
+    key="bmc-widget"
+    data-name="BMC-Widget"
+    data-cfasync="false"
+    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+    data-id="logappradep"
+    data-description="Support me on Buy me a coffee!"
+    data-message=""
+    data-color="#40DCA5"
+    data-position="Right"
+    data-x_margin="18"
+    data-y_margin="18"
+></script>,
+]);
+};
